b5cf13b48625907a0d7a2d0a92fc2e29f59c6a70
Claudio Spizzi <claudio@spizzi.net>
2021-03-31 20:34:47 +0200

    Fixed: Missing export for InfluxDb2

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 export * from './functions/ConfigParser';
 export * from './helpers/Mutex';
 export * from './modules/InfluxDb';
+export * from './modules/InfluxDb2';
 export * from './modules/LoxoneMiniserver';
 export * from './modules/MqttBroker';
 export * from './modules/SmartHomeDevice';
